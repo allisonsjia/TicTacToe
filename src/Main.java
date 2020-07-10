12eb601d4e8a0c2d94e2b71807e6342049ad616f
Angela Jia <42385351+22angiej@users.noreply.github.com>
2020-07-10 00:52:40 -0700

    Avoid merge conflict again

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 public class Main 
 {    public static void main(String[] args)
     {
-        System.out.println("Avoiding Merge Conflict!");
+        System.out.println("Avoiding Merge Conflict");
     }
 }
