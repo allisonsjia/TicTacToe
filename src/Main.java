42091cdeaed8f6f594636d351d94ccd6cfc44f00
Angela Jia <42385351+22angiej@users.noreply.github.com>
2020-07-10 00:45:06 -0700

    Avoid merge conflict

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 public class Main 
 {    public static void main(String[] args)
     {
-        System.out.println("Allison Rocks!");
+        System.out.println("Avoiding Merge Conflict!");
     }
 }
